abffdd9808c669515a73aa998a4ec7f6bd9549f0
wjw465150 <wjw465150@gmail.com>
2023-04-05 09:23:22 +0800

    使用Redis来作为Cluster Manager!

--- src/main/java/org/wjw/vertx/performance/WithClusterStarter.java
+++ src/main/java/org/wjw/vertx/performance/WithClusterStarter.java
@@ -1,15 +1,17 @@
 package org.wjw.vertx.performance;
 
+import java.io.IOException;
+
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 import io.vertx.core.spi.cluster.ClusterManager;
-import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
+import io.vertx.spi.cluster.redis.RedisClusterManager;
 
 public class WithClusterStarter extends AbstractVertxStarter {
 
-  public static void main(String... args) {
+  public static void main(String... args) throws Exception {
     System.out.println("Starting in clustered mode");
-    ClusterManager mgr = new HazelcastClusterManager();
+    ClusterManager mgr = new RedisClusterManager();
 
     VertxOptions options = new VertxOptions().setClusterManager(mgr);
     Vertx.clusteredVertx(options, resultHandler -> {
